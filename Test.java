7984fbc767e0b845c97a14b2a8739ab1930cb74c
Sumit <sumitkhandale73@gmail.com>
2022-07-16 10:49:08 +0530

    added message in Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
 public static void main(String[] args) {
     int x = 400;
     int y = 100;
-    System.out.println(x + y);
+    System.out.println("The Sum of x and y : " + x + y);
 }
     
 }
